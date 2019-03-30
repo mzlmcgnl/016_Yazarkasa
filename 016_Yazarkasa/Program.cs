1e39ccc00be7a4dc0739c88f62cda0960bed0d20
mazlum.caginli <mazlum.caginli@SMRT405-06>
2019-03-30 12:22:56 +0300

    Add project files.

---
+++ 016_Yazarkasa/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _016_Yazarkasa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal Bakiye = 100,
+                    Tutar;
+            string[] Hareketler = null,
+                     Yedek = null;
+            anaMenu:
+
+            Console.Clear();
+            Console.WriteLine("╔═══════════════════════════════════════════════════════════════╗");
+            Console.WriteLine("║                          Bankamatik                           ║");
+            Console.WriteLine("╠═══════════════════════════════════════════════════════════════╣");
+            Console.WriteLine("║ 1. Para Yatır                                                 ║");
+            Console.WriteLine("║ 2. Para Çek                                                   ║");
+            Console.WriteLine("║ 3. Hareketler                                                 ║");
+            Console.WriteLine("║ 4. Çıkış                                                      ║");
+            Console.WriteLine("╚═══════════════════════════════════════════════════════════════╝");
+
+            Console.WriteLine("Bir Menü Seçiniz");
+
+            switch (Console.ReadLine())
+            {
+
+                case "1":
+                    paraYatir:
+                    Console.Clear();
+                    Console.WriteLine("╔═══════════════════════════════════════════════╗");
+                    Console.WriteLine("║                 Para Yatır                    ║");
+                    Console.WriteLine("╚═══════════════════════════════════════════════╝");
+
+                    Console.Write("Yatırmak istediğiniz miktarı giriniz : ");
+
+                    Tutar = decimal.Parse(Console.ReadLine());
+
+                    if (Hareketler == null)
+                        Hareketler = new string[1];
+                    else
+                    {
+                        Yedek = Hareketler;
+                        Hareketler = new string[Hareketler.Length + 1];
+                        for (int i = 0; i < Yedek.Length; i++) Hareketler[i] = Yedek[i];
+                    }
+                    Hareketler[Hareketler.Length - 1] = $"+\t{Tutar}\t{DateTime.Now.ToShortTimeString()}";
+                    Bakiye += Tutar;
+
+                    Console.WriteLine($"Hesabınızda şuan {Bakiye} TL vardır\nBaşka işlem yapmak ister misiniz <e/h> ");
+                    secim:
+                    switch (Console.ReadLine().ToLower())
+                    {
+                        case "e": goto paraYatir;
+                        case "h": goto anaMenu;
+                        default: goto secim;
+                    }
+
+                case "2":
+                    paraCek:
+                    Console.Clear();
+                    Console.WriteLine("╔═══════════════════════════════════════════════╗");
+                    Console.WriteLine("║                 Para Çek                      ║");
+                    Console.WriteLine("╚═══════════════════════════════════════════════╝");
+
+                    Console.Write("Çekmek istediğiniz miktarı giriniz : ");
+
+                    Tutar = decimal.Parse(Console.ReadLine());
+
+                  
+
+                    if (Tutar <= Bakiye)
+                    {
+                        if (Hareketler == null)
+                            Hareketler = new string[1];
+                        else
+                        {
+                            Yedek = Hareketler;
+                            Hareketler = new string[Hareketler.Length + 1];
+                            for (int i = 0; i < Yedek.Length; i++) Hareketler[i] = Yedek[i];
+                        }
+                        Hareketler[Hareketler.Length - 1] = $"-\t{Tutar}\t{DateTime.Now.ToShortTimeString()}";
+                        Bakiye -= Tutar;
+
+
+                        Console.WriteLine($"Hesabınızda şuan {Bakiye} TL vardır\nBaşka işlem yapmak ister misiniz <e/h> ");
+                        secim1:
+                        switch (Console.ReadLine().ToLower())
+                        {
+                            case "e": goto paraCek;
+                            case "h": goto anaMenu;
+
+                            default: goto secim1;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Hesabınızda yeterli bakiye bulunmamaktadır\nBakiyeniz : {Bakiye}\nTekrar denemek ister misiniz? <e/h>");
+                        secim2:
+                        switch (Console.ReadLine().ToLower())
+                        {
+                            case "e": goto paraCek;
+                            case "h": goto anaMenu;
+                            default: goto secim2;
+                        }
+                    }
+
+                case "3":
+                    Console.Clear();
+                    Console.WriteLine("╔═══════════════════════════════════════════════════════╗");
+                    Console.WriteLine("║                      Hareketler                       ║");
+                    Console.WriteLine("╚═══════════════════════════════════════════════════════╝");
+
+                    Console.WriteLine("TİP\tTUTAR\tTARİH");
+                    Console.WriteLine("═════════════════════════════════════════════════════════");
+
+                    foreach (var item in Hareketler)
+                    {
+                        Console.WriteLine(item);
+                    }
+                    break;
+                case "4": break;
+                default:
+                    goto anaMenu;
+
+                    
+            }
+            Thread.Sleep(2000);
+        }
+    }
+}
---
+++ 016_Yazarkasa/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("016_Yazarkasa")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("016_Yazarkasa")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2aafd819-0cb6-4787-9a74-9f8338488ae8")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
